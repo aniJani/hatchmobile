9527ee1a9872cfd6971bbeb056cafbdcf5d0897e
Zenith <96979639+appyjuss@users.noreply.github.com>
2024-11-09 13:06:47 -0600

    fetch project

--- services/projectServices.js
+++ services/projectServices.js
@@ -2,15 +2,23 @@
 import axios from 'axios';
 
 // Ensure you correctly format the URL using template literals
-export const loadProjects = async () => {
-  try {
-    const response = await axios.get(`http://${process.env.BASE_URL}/project/list`);
-    return response.data;
-  } catch (error) {
-    console.error("Error loading projects:", error);
-    throw error;
-  }
-};
+// Function to load projects by user email
+export const loadProjects = async (email) => {
+    try {
+      // Construct the base URL using an environment variable and template literals
+        const response = await axios.get(`http://${process.env.BASE_URL}/projects/list`, {
+        params: { email }, // Pass the email as a query parameter
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      return response.data.projects; // Return the list of projects
+    } catch (error) {
+      console.error("Error loading projects:", error);
+      throw error; // Re-throw the error for further handling if needed
+    }
+  };
 
 export const loadTasks = async () => {
   try {
